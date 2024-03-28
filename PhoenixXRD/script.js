079b2f6fc59f5fb0c6f33ff84dc11e2b81e8c3a6
moisur <159698425+moisur@users.noreply.github.com>
2024-03-28 17:47:36 +0100

    Update script.js

--- PhoenixXRD/script.js
+++ PhoenixXRD/script.js
@@ -1,20 +1,3 @@
-const images = ["crystal-1.png", "crystal-2.png", "logo.png"];
-let index = 0;
-let counter = 0;
-const imageElement = document.getElementById('image');
-
-function changeImage() {
-    imageElement.src = images[index];
-    index = (index + 1) % images.length;
-    counter++;
-
-    if (counter === 9) {
-        clearInterval(intervalID); // Arrête l'intervalle après 3 itérations
-    }
-}
-
-const intervalID = setInterval(changeImage, 1000); // Change l'image toutes les 2 secondes (2000 millisecondes)
-
 const projectBubbles = document.querySelectorAll('.project-bubble');
 
 const observer = new IntersectionObserver((entries) => {
@@ -28,3 +11,66 @@ const observer = new IntersectionObserver((entries) => {
 projectBubbles.forEach((bubble) => {
   observer.observe(bubble);
 });
+
+
+/*PETITS BONHOMMES QUI BOUGENT*/
+const element1 = document.querySelector('.element1');
+const element2 = document.querySelector('.element2');
+const element3 = document.querySelector('.element3');
+
+let screenWidth = window.innerWidth;
+let screenHeight = window.innerHeight;
+
+// Élément 1 : de gauche à droite à 25% de l'écran
+let element1Left = 0;
+let element1Right = screenWidth * 0.25;
+
+// Élément 2 : de droite à gauche à 50% de l'écran
+let element2Left = screenWidth;
+let element2Right = screenWidth * 0.5;
+
+// Élément 3 : de gauche à droite à 75% de l'écran
+let element3Left = 0;
+let element3Right = screenWidth * 0.75;
+
+let animationStarted = false;
+
+window.addEventListener('scroll', function() {
+  const maxWidthSection = document.querySelector('.max-width-section');
+  const maxWidthSectionTop = maxWidthSection.getBoundingClientRect().top;
+
+  if (maxWidthSectionTop <= screenHeight && !animationStarted) {
+    animationStarted = true;
+    moveElements();
+  }
+});
+
+function moveElements() {
+  // Élément 1
+  if (element1.offsetLeft < element1Right) {
+    element1.style.left = element1Left + 'px';
+    element1Left += 2;
+  } else {
+    element1Left = 0;
+  }
+
+  // Élément 2
+  if (element2.offsetLeft > element2Right) {
+    element2.style.left = element2Left + 'px';
+    element2Left -= 2;
+  } else {
+    element2Left = screenWidth;
+  }
+
+  // Élément 3
+  if (element3.offsetLeft < element3Right) {
+    element3.style.left = element3Left + 'px';
+    element3Left += 2;
+  } else {
+    element3Left = 0;
+  }
+
+  if (animationStarted) {
+    requestAnimationFrame(moveElements);
+  }
+}
