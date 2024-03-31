8a5621b19c31d39f29ac79581e447904f549784a
moisur <159698425+moisur@users.noreply.github.com>
2024-03-31 17:13:38 +0200

    Add files via upload

---
+++ PhoenixXRD/socials.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const telegramButton = document.getElementById('telegramButton');
+    const twitterButton = document.getElementById('twitterButton');
+
+    telegramButton.addEventListener('click', function() {
+        window.location.href = 'https://t.me/+PLzGA6OtJCo2NzM0';
+    });
+
+    twitterButton.addEventListener('click', function() {
+        window.location.href = 'https://twitter.com/Phoenix_XRD';
+    });
+});
