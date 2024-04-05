5daff6c7d2180e4000532246d5071d6746c206f1
moisur <159698425+moisur@users.noreply.github.com>
2024-04-05 20:48:09 +0200

    Add files via upload

--- PhoenixXRD/script.js
+++ PhoenixXRD/script.js
@@ -78,75 +78,31 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
   });
 });
+// Sélection des éléments nécessaires
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+const navLinks = document.querySelectorAll('.nav-links a');
 
-// Données pour les boîtes de la timeline
-const timelineData = [
-  {
-    title: "4 Millions tokens burned",
-    image: "image1.jpg",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-  },
-  {
-    title: "10 % airdrop already done",
-    image: "image2.jpg",
-    description: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-  },
-  {
-    title: "NFT Collection comming",
-    image: "image3.jpg",
-    description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-  },
-  {
-    title: "Phoenix Games",
-    image: "image4.jpg",
-    description: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-  }
-];
-
-
-// Sélectionnez les éléments HTML correspondants
-const timelineColElements = document.querySelectorAll(".timeline-col");
-const descriptionElements = document.querySelectorAll(".description");
-
-// Fonction pour animer le texte
-function animateText(element, text, delay = 50) {
-  let currentText = "";
-  element.textContent = "";
-
-  const interval = setInterval(() => {
-    currentText += text.charAt(currentText.length);
-    let color = currentText.length % 3 === 0 ? "--jc" : currentText.length % 3 === 1 ? "--jf" : "--jc";
-    element.style.color = `var(${color})`;
-    element.innerHTML = `${currentText.slice(0, -1)}<span class="glow-shadow">${currentText.slice(-1)}</span>`;
-
-    if (currentText === text) {
-      clearInterval(interval);
-    }
-  }, delay);
+// Fonction pour fermer le menu
+function closeMenu() {
+  navbarCollapse.classList.remove('show');
 }
 
-// Fonction pour animer les colonnes
-function animateColumns() {
-  timelineColElements.forEach((col, index) => {
-    setTimeout(() => {
-      const descriptionElement = col.querySelector(".description");
-      const description = timelineData[index].description;
-      animateText(descriptionElement, description);
-    }, index * 500); // Délai de 500ms entre chaque colonne
-  });
-}
-
-// Appliquez les données à chaque boîte de la timeline
-timelineData.forEach((data, index) => {
-  const colElement = timelineColElements[index];
-  const titleElement = colElement.querySelector(".title");
-  const descriptionElement = colElement.querySelector(".description");
-  const imageElement = colElement.querySelector("img");
-
-  titleElement.textContent = data.title;
-  descriptionElement.textContent = "";
-  imageElement.src = data.image;
+// Ajout d'un événement de clic sur le bouton de menu
+navbarToggler.addEventListener('click', () => {
+  navbarCollapse.classList.toggle('show');
 });
 
-// Lancez l'animation des colonnes
-animateColumns();
+// Ajout d'un événement de clic sur les liens du menu
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    closeMenu();
+  });
+});
+
+// Ajout d'un événement de clic sur le document pour fermer le menu
+document.addEventListener('click', (event) => {
+  if (!event.target.closest('.navbar-nav') && !event.target.closest('.navbar-toggler')) {
+    closeMenu();
+  }
+});
